83117fcca2c563017c1ebbcbcd6399b3352214ea
Ankit Kumar <ankit.imaddi47@gmail.com>
2025-11-22 05:45:45 +0530

    feat: keeping socket mode optional based on slack api settings for socket mode

--- src/server.js
+++ src/server.js
@@ -12,17 +12,19 @@ const receiver = new ExpressReceiver({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   endpoints: {
     commands: '/slack/commands', // Slack will POST here for slash commands
-    events: '/slack/events',     // events endpoint (default)
-    interactive: '/slack/interactive' // button press etc.
-  }
+    events: '/slack/events', // events endpoint (default)
+    interactive: '/slack/interactive', // button press etc.
+  },
 });
 
 // initialize your app with the receiver
 const app = new App({
-    token: process.env.SLACK_OAUTH_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    receiver
-    // logLevel: 'debug',
+  token: process.env.SLACK_OAUTH_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  // logLevel: 'debug',
+  ...(process.env.SOCKET_MODE == 'true'
+    ? { socketMode: true, appToken: process.env.SLACK_APP_TOKEN }
+    : { receiver }),
 });
 
 // register handlers
@@ -32,12 +34,12 @@ await registerInteractiveHandlers(app);
 
 // Start the app server
 (async () => {
-    try {
-        const PORT = process.env.PORT || 3000;
-        await app.start(PORT);
-        console.log(`⚡️ Slack bot is running on port ${PORT}!`);
-    } catch (error) {
-        console.error('Failed to start app:', error);
-        process.exit(1);
-    }
+  try {
+    const PORT = process.env.PORT || 3000;
+    await app.start(PORT);
+    console.log(`⚡️ Slack bot is running on port ${PORT}!`);
+  } catch (error) {
+    console.error('Failed to start app:', error);
+    process.exit(1);
+  }
 })();
